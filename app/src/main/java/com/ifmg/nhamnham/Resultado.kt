cf14a73de5a83eda10ef891dd6aafdca16485b30
GabOof <oliveirafonseca.gab@gmail.com>
2024-12-29 16:10:29 -0300

    Retorna o resultado na tela e atualiza o placar do jogo

--- app/src/main/java/com/ifmg/nhamnham/Resultado.kt
+++ app/src/main/java/com/ifmg/nhamnham/Resultado.kt
@@ -20,19 +20,40 @@ class Resultado : AppCompatActivity() {
         binding = ActivityResultadoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configura o listener de ajuste de padding para a tela
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        // Obtém o resultado do jogo vindo do Intent
+        val resultadoDoJogo = intent.getStringExtra("vencedor") ?: "Erro ao obter resultado"
+
+        // Obtenha o placar vindo do Intent, caso já tenha sido passado
+        val placarJogador1 = intent.getIntExtra("placarJogador1", 0)
+        val placarJogador2 = intent.getIntExtra("placarJogador2", 0)
+
+        // Exibe o resultado no formato desejado
+        binding.txtResultado.text = "$resultadoDoJogo ganhou! 🥳"
+
+        // Atualiza o placar
+        binding.txtPlacar.text = "Placar: $placarJogador1 x $placarJogador2"
+
+        // Ações dos botões
         binding.btnRetultadoVoltarInicio.setOnClickListener {
+            // Quando voltar à tela inicial, o placar é zerado
             val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("placarJogador1", 0)
+            intent.putExtra("placarJogador2", 0)
             startActivity(intent)
         }
 
         binding.btnReiniciar.setOnClickListener {
+            // Reinicia o jogo, mantendo o placar anterior
             val intent = Intent(this, Jogo::class.java)
+            intent.putExtra("placarJogador1", placarJogador1)
+            intent.putExtra("placarJogador2", placarJogador2)
             startActivity(intent)
         }
     }
